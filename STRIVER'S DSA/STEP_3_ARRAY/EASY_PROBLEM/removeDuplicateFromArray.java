56faf166a0f9efa5c349f666bb9901e1e21bb7df
Saurabh Patel <saurabhpatel4589@gmail.com>
2024-08-31 08:48:13 +0530

    DSA WITH JAVA

---
+++ STRIVER'S DSA/STEP_3_ARRAY/EASY_PROBLEM/removeDuplicateFromArray.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class removeDuplicateFromArray{
+    public static int remove_Duplicate_From_Sorted_Array(int arr[]){
+       int i=0;
+        for(int j=1;j<arr.length;j++){
+            if(arr[i]!=arr[j]){
+                arr[i+1]=arr[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of the array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements in your array");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The elements in array are:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println(remove_Duplicate_From_Sorted_Array(arr));
+
+    }
+}
