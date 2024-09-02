6aaa0315c851f11a56f5e9f7c8011cca1442d299
Saurabh Patel <saurabhpatel4589@gmail.com>
2024-09-02 21:42:24 +0530

    DSA WITH JAVA

---
+++ STRIVER'S DSA/STEP_3_ARRAY/EASY_PROBLEM/findMissingNumberInArray.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class findMissingNumberInArray {
+
+    public static int missingNumber(int arr[],int n){
+        int sum_1=0;
+        int sum_2=0;
+        for(int i=0;i<n+1;i++){
+            sum_1+=i;
+        }
+        for(int i=0;i<n;i++){
+            sum_2+=arr[i];
+        }
+        int difference = sum_1-sum_2;
+        return  difference;
+    }
+
+    public static int bruteMissingNumber(int arr[]){
+        //T.C =N*N   S.C=O(1)
+        for(int i=1;i<=arr.length;i++){
+            int flag = 0;
+            for(int j=0;j<arr.length;j++){
+                if(arr[j]==i){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0){
+                return i;
+            }
+        }
+        return 1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of the array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements in your array");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The elements in array are:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println(bruteMissingNumber(arr));
+
+    }
+}
---
+++ STRIVER'S DSA/STEP_3_ARRAY/EASY_PROBLEM/findTheIntersectionOfTwoArray.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class findTheIntersectionOfTwoArray {
+    public static ArrayList<Integer> bruteIntersection(int arr1[], int arr2[], int n, int m) {
+        ArrayList<Integer> intersect = new ArrayList<>(); // T.C=O(N*M) S.C=O(M)
+        int visited[] = new int[m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr1[i] == arr2[j] && visited[j] == 0) { // agar array 1 ka value aray 2 ke barabar hai to or wo
+                                                             // visit nhi hua hai to add krnege intersect mein
+                    intersect.add(arr1[i]);
+                    visited[j] = 1;
+                    break;
+                }
+                if (arr2[j] > arr1[i]) {
+                    break;
+                }
+            }
+        }
+        return intersect;
+    }
+
+    public static ArrayList<Integer> optimalIntersect(int a[],int b[],int n,int m){
+           ArrayList<Integer>intersect=new ArrayList<>();               //T.C=O(N+M)     SC=O(1)
+           int i=0,j=0;
+           while(i<n && j<m){
+            if(a[i]>b[j]){
+                j++;
+            }
+            else if(a[i]<b[j]){
+                i++;
+            }
+            else{
+                intersect.add(a[i]);
+                i++;
+                j++;
+            }
+           }
+           return intersect;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Get the size of the first array from the user
+        System.out.print("Enter the number of elements in the first array: ");
+        int n = scanner.nextInt();
+        int[] arr_1 = new int[n];
+
+        // Get the elements of the first array from the user
+        System.out.println("Enter the elements of the first array:");
+        for (int i = 0; i < n; i++) {
+            arr_1[i] = scanner.nextInt();
+        }
+
+        // Get the size of the second array from the user
+        System.out.print("Enter the number of elements in the second array: ");
+        int m = scanner.nextInt();
+        int[] arr_2 = new int[m];
+
+        // Get the elements of the second array from the user
+        System.out.println("Enter the elements of the second array:");
+        for (int i = 0; i < m; i++) {
+            arr_2[i] = scanner.nextInt();
+        }
+
+        ArrayList<Integer> storage = optimalIntersect(arr_1, arr_2, n, m);
+        for (int i : storage) {
+            System.out.print(i + " ");
+        }
+    }
+}
