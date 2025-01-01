906bcea561efae8ee97cffefb63323b45b0f43cc
Saurabh Patel <139550710+18-saurabh@users.noreply.github.com>
2025-01-01 12:18:57 +0530

    DSA WITH JAVA

--- STRIVER'S DSA/STEP_6_LEARN_LINKED_LIS/1_SINGLY_LINKED_LIST_INTRODUCTION/2_MEDIUM_PROBLEM/_10_sortLL.java
+++ STRIVER'S DSA/STEP_6_LEARN_LINKED_LIS/1_SINGLY_LINKED_LIST_INTRODUCTION/2_MEDIUM_PROBLEM/_10_sortLL.java
@@ -36,5 +36,52 @@ public class _10_sortLL {
         }
         return head;
     }
+      public ListNode findMiddle(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // Split the list into two halves
+        ListNode middle = slow.next;
+        slow.next = null; // Disconnect the left half from the right half
+        return middle;
+    }
+
+    public ListNode mergeTwoList(ListNode list1, ListNode list2) {
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+        // Attach the remaining nodes from either list
+        if (list1 != null) {
+            temp.next = list1;
+        } else if (list2 != null) {
+            temp.next = list2;
+        }
+        return dummyNode.next;
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode middleNode = findMiddle(head);
+        ListNode leftHead = sortList(head);        // Sort the left half
+        ListNode rightHead = sortList(middleNode); // Sort the right half
+        return mergeTwoList(leftHead, rightHead);  // Merge the sorted halves
+    }
+
 
 }
