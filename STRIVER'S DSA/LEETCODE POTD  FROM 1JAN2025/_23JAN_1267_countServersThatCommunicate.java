e897fa98d6a586029975289ef4756339dc1b094a
Saurabh Patel <139550710+18-saurabh@users.noreply.github.com>
2025-01-23 17:35:29 +0530

    DSA WITH JAVA

---
+++ STRIVER'S DSA/LEETCODE POTD  FROM 1JAN2025/_23JAN_1267_countServersThatCommunicate.java
@@ -0,0 +1,26 @@
+public class _23JAN_1267_countServersThatCommunicate {
+    public int countServers(int[][]grid){
+        int n=grid.length;
+        int m=grid[0].length;
+        int []rowCount=new int[n];
+        int[]colCount=new int[m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    rowCount[i]+=1;
+                    colCount[j]+=1;
+                }
+            }
+        }
+        int count=0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1 &&(rowCount[i]>1||colCount[j]>1)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+
+    }
+}
