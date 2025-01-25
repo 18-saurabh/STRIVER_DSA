e890c38647d5414552fad250b5f0900ea829bfab
Saurabh Patel <139550710+18-saurabh@users.noreply.github.com>
2025-01-25 09:50:50 +0530

    DSA WITH JAVA

--- STRIVER'S DSA/STEP_8_BIT_MANIPULATION/1_EASY_PROBLEM/_4_bitManipulation.java
+++ STRIVER'S DSA/STEP_8_BIT_MANIPULATION/1_EASY_PROBLEM/_4_bitManipulation.java
@@ -1,20 +1,8 @@
 public class _4_bitManipulation {
-    static String convertToBinary(int num){
-        StringBuilder ans=new StringBuilder();
-        while(num>0){
-            if(num%2==1)ans.append(1);
-            else ans.append(0);
-            num=num/2;
-        }
-        return ans.reverse().toString();
-    }
-
     static void bitManipulation(int num, int i) {
-        // code here
-        StringBuilder binaryChar = new StringBuilder(convertToBinary(num));
-        int len = binaryChar.length();
-        if (binaryChar.charAt((len + 1) - i) == '0') { //settting bit
-            binaryChar.setCharAt((len + 1) - i, '1');
-        }
+        int getBit = (int) ((num >> (i - 1)) & 1);
+        long setBit = num | (1L << (i - 1));
+        long clearBit = num & ~(1L << (i - 1));
+        System.out.print(getBit + " " + setBit + " " + clearBit);
     }
 }
