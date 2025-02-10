1e385f6ff515ddf4e725db1f74289e55b442f410
18-saurabh <saurabhpatel4589@gmail.com>
2025-02-10 17:39:43 +0530

    DSA WITH JAVA

---
+++ STRIVER'S DSA/LEETCODE POTD 2025/_2_FEBRUARY_2025/_9FEB_3174_clearDigits.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class _9FEB_3174_clearDigits {
+     public String clearDigits(String s) {
+       Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                // Remove the closest non-digit character to its left
+                while (!stack.isEmpty() && Character.isLetter(stack.peek())) {
+                    stack.pop();
+                    break;  // Only remove one letter
+                }
+            } else {
+                stack.push(ch);  // Push letters onto stack
+            }
+        }
+
+        // Convert stack back to string
+        StringBuilder result = new StringBuilder();
+        for (char ch : stack) {
+            result.append(ch);
+        }
+        
+        return result.toString();  
+    }
+}
