7df35c5cca945eeaa1cd36b3651843ca5cda4f0f
18-saurabh <saurabhpatel4589@gmail.com>
2025-04-28 13:19:21 +0530

    DSA WITH JAVA

---
+++ STRIVER'S DSA/STEP_9_STACK_&_QUEUE/1_LEARNING/arrayStack.java
@@ -0,0 +1,42 @@
+public class arrayStack {
+    private int top;
+    private int[] st_arr;
+
+    public arrayStack() {
+        top = -1;
+        st_arr = new int[10];
+    }
+
+    public void push(int x) {
+        if (top >= 9) {
+            System.out.println("Stack is overflow");
+        } else {
+            top++;
+            st_arr[top] = x;
+        }
+    }
+
+    public int pop() {
+        if (top == -1) {
+            System.out.println("Stack is Underflow");
+            return -1;
+        } else {
+            int poppedElement = st_arr[top];
+            top--;
+            return poppedElement;
+        }
+    }
+
+    public int peek() {
+        if (top == -1) {
+            System.out.println("Stack is empty");
+            return -1;
+        } else {
+            return st_arr[top];
+        }
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+}
