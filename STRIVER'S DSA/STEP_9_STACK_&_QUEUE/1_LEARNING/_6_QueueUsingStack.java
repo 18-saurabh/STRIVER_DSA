816c00d11396b6d729dae39c2a2fbeaad79c5ca1
18-saurabh <saurabhpatel4589@gmail.com>
2025-04-30 17:38:50 +0530

    DSA WITH JAVA

--- STRIVER'S DSA/STEP_9_STACK_&_QUEUE/1_LEARNING/_6_QueueUsingStack.java
+++ STRIVER'S DSA/STEP_9_STACK_&_QUEUE/1_LEARNING/_6_QueueUsingStack.java
@@ -1,3 +1,25 @@
+import java.util.Stack;
 public class _6_QueueUsingStack {
-    
+    Stack<Integer>s1=new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+    public void push(int x){
+        while (s1.size()!=0) {
+            s2.push(s1.peek());
+            s1.pop();
+        }
+        s1.push(x);
+        while (s2.size()!=0) {
+            s1.push(s2.peek());
+            s2.pop();
+        }
+    }
+    public int pop(){
+        return s1.pop();
+    }
+    public int peek(){
+        return s1.peek();
+    }
+    public boolean isEmpty(){
+        return s1.isEmpty();
+    }
 }
