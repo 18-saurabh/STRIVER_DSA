76ada2f49ca606ca7bb74bedc06ea30f32bf3e1d
18-saurabh <saurabhpatel4589@gmail.com>
2025-05-01 12:48:36 +0530

    DSA WITH JAVA

---
+++ STRIVER'S DSA/STEP_9_STACK_&_QUEUE/1_LEARNING/_7_validParanthesis.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+public class _7_validParanthesis {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char it : s.toCharArray()) {
+            if (it == '(' || it == '{' || it == '[') {
+                st.push(it);
+            } else {
+                if (st.isEmpty()) return false;
+                char ch = st.pop();
+                if (ch == '(' && it == ')' || ch == '{' && it == '}' || ch == '[' && it == ']') continue;
+                else return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
