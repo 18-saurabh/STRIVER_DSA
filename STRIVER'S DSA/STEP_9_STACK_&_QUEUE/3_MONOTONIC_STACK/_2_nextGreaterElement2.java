1e9b8bb4d6e8f3b41eb1e5f11144e79e82cce98b
Saurabh Patel <saurabhpatel4589@gmail.com>
2025-06-03 10:19:13 +0530

    DSA WITH JAVA

--- STRIVER'S DSA/STEP_9_STACK_&_QUEUE/3_MONOTONIC_STACK/_2_nextGreaterElement2.java
+++ STRIVER'S DSA/STEP_9_STACK_&_QUEUE/3_MONOTONIC_STACK/_2_nextGreaterElement2.java
@@ -1,18 +1,36 @@
+import java.util.Stack;
+
 public class _2_nextGreaterElement2 {
-     public int[] brute_nextGreaterElements(int[] nums) {
-        int n=nums.length;
-        int nge[]=new int [n];
-        for(int i=0;i<n;i++){
-            nge[i]=-1;
-            for(int j=1;j<n;j++){
-                int ind=(i+j)%n;
-                if(nums[ind]>nums[i]){
-                    nge[i]=nums[ind];
+    public int[] brute_nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int nge[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            nge[i] = -1;
+            for (int j = 1; j < n; j++) {
+                int ind = (i + j) % n;
+                if (nums[ind] > nums[i]) {
+                    nge[i] = nums[ind];
                     break;
                 }
             }
         }
         return nge;
     }
-    
+
+    public int[] stack_nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
+        int nge[] = new int[n];
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums[i % n]) {
+                stack.pop();
+            }
+            if (i < n) {
+                nge[i] = stack.isEmpty() ? -1 : stack.peek();
+            }
+            stack.push(nums[i % n]);
+        }
+        return nge;
+    }
+
 }
