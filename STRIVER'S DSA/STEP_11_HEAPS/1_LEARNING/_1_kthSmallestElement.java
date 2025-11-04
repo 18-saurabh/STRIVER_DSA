b81c071dfbe6b8f4954d966abb50045cd9220717
Saurabh Patel <saurabhpatel4589@gmail.com>
2025-11-04 19:48:31 +0530

    Add kth smallest element finder using max heap
    Introduced _1_kthSmallestElement.java with a method to find the kth smallest element in an array using a max heap approach.

---
+++ STRIVER'S DSA/STEP_11_HEAPS/1_LEARNING/_1_kthSmallestElement.java
@@ -0,0 +1,16 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class _1_kthSmallestElement {
+     public int kthSmallest(int[] arr, int k) {
+        // Code here
+        PriorityQueue<Integer>maxHeap=new PriorityQueue<>(Collections.reverseOrder());
+        for(int num: arr){
+            maxHeap.add(num);
+            if(maxHeap.size()>k){
+                maxHeap.poll();
+            }
+        }
+        return maxHeap.peek();
+    }
+}
